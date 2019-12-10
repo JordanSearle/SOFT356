d5d0892bf14c671f3686183013662fb593b4624d
Jordan Searle <32457721+JordanSearle@users.noreply.github.com>
2019-12-10 15:19:51 +0000

    Updated the server var
    Added a time stamp, which will be repeated on each console.log command.

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
+
 app.get('/', function (req, res) {
   res.status(200).send('ok');
 });
+
 var server = app.listen(9000, function () {
   var port = server.address().port;
-  console.log('Example app listening at port %s', port);
+  console.log('%s Listening at port %s', new Date(), port);
 });
+
 module.exports = server;

b473e6769fe8306f26af8ef34749d4287fd94487
Jordan Searle <32457721+JordanSearle@users.noreply.github.com>
2019-12-15 17:11:51 +0000

    Corrected 404 errors for db reading
    The server will now only respond with a 404 message if the server cannot be found.

--- client/templates/login.js
+++ client/templates/login.js
@@ -7,8 +7,12 @@ loginModule.component("login",{
       console.log("Starting Login Script");
     }
     $scope.login = function() {
-      console.log("login blah blah,.,");
-      $http.post("/login",{username:Email,password:Password})
+        $http.post('http://localhost:9000/login',$scope.formData).
+         then(function(response) {
+             console.log(response);
+         }).catch(function(response) {
+             console.error(response.data+" error in posting");
+         })
     }
   }
 })
--- server.js
+++ server.js
@@ -4,9 +4,11 @@ var schemas = require("./schemas");
 var app = express();
 var bodyParser = require('body-parser');
 
+
 uri = 'mongodb://localhost:27017/soft355';
 
 app.use(express.static("client"));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}) );
 
@@ -14,6 +16,7 @@ app.get("/", function(request, response) {
   response.status(200).sendFile("/", {root: "client"});
 });
 
+//Just test code can be removed.
 app.get('/writetodb',function(req,res) {
   var User = schemas.User;
   var newUser = new User({userID:123,username:"test",password:"test"});
@@ -26,33 +29,32 @@ app.get('/writetodb',function(req,res) {
   });
 //Currently not working I think?
 });
+//Login System
 app.post('/login',function(req,res) {
-  console.log("recieved a Post request");
   var user = schemas.User;
-  var email = req.body.Email;
-  var password = req.body.Password;
+  var email = req.body.email;
+  var password = req.body.password;
+  console.log(email +' ' + password);
 
   user.findOne({username: email}, function(err,obj) {
-    if(err) {console.log(err);res.status(404);};
-    if(obj===null){res.status(404).send("ok");}
+    if(err) {console.log(err);res.status(404).send("Failed To connect to Database");};
+    if(obj===null){res.status(200).send("Failed to FInd values");}
     else{
       //Needs to  be chaged for session values... but can be done later.
     console.log(obj);
     res.status(200).send(obj);
   }
   });
-
-
 });
 
-
+//Server Start...
 var server = app.listen(9000, function () {
   // Connect to Mongoose.
   mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true}).then((test) => {
     console.log("%s Connected to DB", new Date());
   });
   var port = server.address().port;
-  console.log('%s Listening at port %s', new Date(), port);
+  console.log('%s Listening on port %s', new Date(), port);
 });
 
 module.exports = server;

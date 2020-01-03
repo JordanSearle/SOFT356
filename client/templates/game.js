4d9b979b1192ba1d7720e5b3147d20b071cd98d7
Jordan Searle <32457721+JordanSearle@users.noreply.github.com>
2020-01-03 14:09:13 +0000

    Update game.js

--- client/templates/game.js
+++ client/templates/game.js
@@ -1,71 +1,77 @@
 var gameModule = angular.module("game",["ngRoute"]);
 
-function drawBoard(store){
-  // Box width
-var bw = 300;
-// Box height
-var bh = 300;
-// Padding
-var p = 10;
-var temp = JSON.parse(store).board;
-
-var canvas = document.getElementById("canvas");
-
-var context = canvas.getContext("2d");
-    for (var x = 100; x <= bw-100; x += 100) {
-        context.moveTo(x, 0);
-        context.lineTo(x, bh);
-
-    }
-
-    for (var x = 100; x <= bh-100; x += 100) {
-        context.moveTo(0, x);
-        context.lineTo(bw, x);
-    }
-    context.strokeStyle = "black";
-    context.stroke();
-    for (var i = 100; i < 301; i+=100) {
-      for (var j = 100; j < 301; j+=100) {
-        var a = (i/100)-1;
-        var b = (j/100)-1;
-        switch (temp[a][b]) {
-          case 5:
-          //Set Position
-          //Draw X
-          context.font = "110px Arial";
-            context.fillText("X", (j-100)+p, i-p);
-
-            break;
-          case 3:
-          //SetPos
-          //Draw O
-          context.fillText("0", (j-100)+15, i-p);
-            break;
-          default:
-          //Do nothing / wipe...
-        }
-
-      }
-    }
-}
-
-function loadArr() {
-  var ws = new WebSocket("ws://localhost:9000/game/update");
-  ws.onopen = function() {
-           // Web Socket is connected, send data using send()
-      ws.send(JSON.stringify({id: "5e008fff9ad0c72bbc35efb9"}));
-      //Update Baord on message recieve
-      ws.onmessage = function (event) {
-        drawBoard(event.data);
-      }
-};
-}
-
 gameModule.component("game",{
   templateUrl:"templates/game.template.html",
   controller: function dashboardController($scope,$http) {
     var canvas = document.getElementById("canvas");
+    function loadArr(canvas) {
+      var ws = new WebSocket("ws://localhost:9000/game/update");
+      ws.onopen = function() {
+               // Web Socket is connected, send data using send()
+               //message is redundant
+          ws.send(JSON.stringify({id: "5e008fff9ad0c72bbc35efb9"}));
+          //Update Baord on message recieve
+          ws.onmessage = function (event) {
+            drawBoard(event.data);
+            var data = JSON.parse(event.data);
+            if(data.hasOwnProperty("winner")){
+              $scope.winner = data.winner;
+              $scope.$apply();
+              var canvas = document.getElementById("canvas");
+              canvas.removeEventListener('mousedown', click);
+            }
+          }
+    };
+    }
+    function drawBoard(store){
+      // Box width
+    var bw = 300;
+    // Box height
+    var bh = 300;
+    // Padding
+    var p = 10;
+    var temp = JSON.parse(store).board;
 
+
+    var context = canvas.getContext("2d");
+        for (var x = 100; x <= bw-100; x += 100) {
+            context.moveTo(x, 0);
+            context.lineTo(x, bh);
+
+        }
+
+        for (var x = 100; x <= bh-100; x += 100) {
+            context.moveTo(0, x);
+            context.lineTo(bw, x);
+        }
+        context.strokeStyle = "black";
+        context.stroke();
+        for (var i = 100; i < 301; i+=100) {
+          for (var j = 100; j < 301; j+=100) {
+            var a = (i/100)-1;
+            var b = (j/100)-1;
+            switch (temp[a][b]) {
+              case 5:
+              //Set Position
+              //Draw X
+              context.font = "110px Arial";
+              context.fillStyle = "red";
+                context.fillText("X", (j-100)+p, i-p);
+
+                break;
+              case 3:
+              //SetPos
+              //Draw O
+              context.fillStyle = "blue";
+              context.fillText("0", (j-100)+15, i-p);
+                break;
+              default:
+              //Do nothing / wipe...
+            }
+
+          }
+        }
+    }
     function getCursorPosition(canvas, event) {
         const rect = canvas.getBoundingClientRect();
         const x = event.clientX - rect.left;
@@ -80,7 +86,7 @@ gameModule.component("game",{
     }
 
     $scope.load = function() {
-      loadArr();
+      loadArr(canvas);
     }
     $scope.move = function(array,user,value) {
       console.log(array+user+value);
@@ -100,7 +106,7 @@ gameModule.component("game",{
           ws.onmessage = function (event) {
             var output = JSON.parse(event);
             if (output.draw == true) {
-              canvas.removeEventListener('mousedown', click(e));
+              canvas.removeEventListener('mousedown', click);
             }
             loadArr();
           }
